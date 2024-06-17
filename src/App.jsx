f8e4fff3daef1c0b4405d5e9e6314941fe6b8a92
Rohith Innamuri <rohith@Rohiths-Air.lan>
2024-06-17 15:14:55 -0400

    Adding editable Player name component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Player from "./components/Player"
 
 function App() {
   return (
-   <p>Coming Soon...</p>
+   <main>
+    <div id = "game-container">
+      <ol id = "players">
+        <Player name= "Player1" symbol = "X"/>
+        <Player name= "Player2" symbol = "O"/>
+      </ol>
+      GAME BOARD
+    </div>
+    LOG 
+   </main>
   )
 }
 
